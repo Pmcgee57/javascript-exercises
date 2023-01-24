16ab38854d7aacac570e6bd3c349aca7f342f0e6
Paul <pmcgee1@binghamton.edu>
2023-01-24 14:50:43 -0500

    palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (array) {
+    const original = array.replace(/[^\w\s\']|_/g, "")
+        .replace(/\s{2,}/g," ")
+        .toLowerCase()
+        .split(" ")
+        .join("");
+    const reverse = original.split("").reverse().join("");
+    return (original === reverse ? true : false);
+    // if (original === reverse) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+    // console.log(original);
+    // console.log(reverse);
 };
 
 // Do not edit below this line
