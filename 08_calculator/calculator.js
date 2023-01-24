773b506f0fbb8d4d64d636b3ee2d31deb70eee4c
Paul <pmcgee1@binghamton.edu>
2023-01-23 19:45:42 -0500

    calc exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(var1, var2) {
+	return var1 + var2
 };
 
-const subtract = function() {
-	
+const subtract = function(var1, var2) {
+	return var1 - var2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+// const multiply = function(array) {
+// if (array.length > 0 ){
+//   return array.reduce((a,b) => a * b);
+// } else {
+//   return 0;
+// }
+// };
 
+const multiply = function(array) { //if array length is greater than zero, multiply the numbers, if not return zero
+  return array.length              // returns T/F
+    ? array.reduce((a,b) => a * b) // if true, multiply the values in the array
+    : 0;  // if false, return 0
+}
+
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
